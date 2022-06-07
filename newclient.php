75c050ec381ce50cdc3d6283ea4064d340c991ae
Koonri <92974237+Koonri@users.noreply.github.com>
2022-06-07 13:33:15 +0200

    or die

--- newclient.php
+++ newclient.php
@@ -37,11 +37,10 @@
 </form>
 
 <?php
-        $q = $db->prepare("SELECT * FROM klient WHERE id = ?");
-        $q->bind_param("i", $clientid);
-        if($q && $q->execute()) {
-        $login = $q->get_result()->fetch_assoc();
+        if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
+            $ss;
         }
+    
 ?>
 
 
@@ -50,27 +49,27 @@
 
 <h1>Rejestracja</h1>
 <form action="newclient.php" method="POST">
-    <label for="email">Email</label><br>
-    <input type="text" name="email" id="email"><br>
+    <label for="email2">Email</label><br>
+    <input type="text" name="email2" id="email2"><br>
     <label for="login2">Login:</label><br>
-    <input type="text" name="login" id="login"><br>
+    <input type="text" name="login2" id="login2"><br>
     <label for="password2">Haslo:</label><br>
-    <input type="text" name="password" id="password"><br>
-    <label for="name">Imie:</label><br>
-    <input type="text" name="name" id="name"><br>
-    <label for="surname">Nazwisko:</label><br>
-    <input type="text" name="surname" id="surname"><br>
+    <input type="text" name="password2" id="password2"><br>
+    <label for="name2">Imie:</label><br>
+    <input type="text" name="name2" id="name2"><br>
+    <label for="surname2">Nazwisko:</label><br>
+    <input type="text" name="surname2" id="surname2"><br>
     <input type="submit" value="Zarejestruj się"><br>
     </label>
 </form>
 <?php
-    if(isset($_POST['login']) && isset($_POST['password'])) {
+    if(isset($_REQUEST['login2'])&&isset($_REQUEST['password2'])) {
         $q = $db->prepare("INSERT INTO klient VALUES (NULL, ?, ?, ?, ?, ?)");
-        $q->bind_param("sssss", $clientemail, $clientlogin, $clientpass, $clientname, $clientsur);
+        $q->bind_param("sssss", $_REQUEST['email2'], $_REQUEST['login2'], $_REQUEST['password2'], $_REQUEST['name2'], $_REQUEST['surname2']);
         $q->execute();
-        echo "lorem";
+        echo "congrats";
     } else {
-        echo "die"; 
+        echo 'or die';
     }
     
 ?>
