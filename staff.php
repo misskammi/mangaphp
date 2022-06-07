b4f9aa2220ea1eeb7bffd7fb31b585b70145c6aa
misskammi <yome@op.pl>
2022-06-07 18:08:30 +0200

    logowanie works

--- staff.php
+++ staff.php
@@ -5,7 +5,7 @@
 <div class="pudlo">
 <form action="staff.php" method="POST">
   <label for="login3">Login:</label><br>
-  <input type="text" name="log" id="log"><br>
+  <input type="text" name="login3" id="login3"><br>
   <label for="haslo3">Haslo:</label><br>
   <input type="text" name="haslo3" id="haslo3"><br>
   <input type="submit" value="Zaloguj się">
@@ -14,12 +14,12 @@
 <?php
          $db = new mysqli("localhost", "root", "", "manga");
          $q = $db->prepare("SELECT * FROM pracownicy WHERE `login` = ? AND haslo = ?");
-         $q->bind_param("ss", $_REQUEST['log'], $_REQUEST['haslo3']);
+         $q->bind_param("ss", $_REQUEST['login3'], $_REQUEST['haslo3']);
          $q->execute();
          $result = $q->get_result();
          if($result->num_rows == 1) {
-             $staffd = $result->fetch_assoc();
-             $d = $staffd['id'];
+             $staffresult = $result->fetch_assoc();
+             $staffid = $staffresult['id'];
              echo '<form action = "staffmode.php" method="post">
              <input type="submit" value="Przejdź do sklepu">
              </form>';
