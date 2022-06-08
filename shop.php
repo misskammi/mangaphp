6a19b752d8ab957ba800ca49dfda20ce09845b28
misskammi <yome@op.pl>
2022-06-08 14:57:37 +0200

    well

--- shop.php
+++ shop.php
@@ -7,27 +7,27 @@
     <div>
     <img src="fotki/manga1.jpg" alt="cit" height="300px">
     <p>cena 20,99zł</p>
-    <input type="text" name="count" id="count" value="">
+    <input type="text" name="count" id="count" value="0">
     </div><div>
     <img src="fotki/manga2.jpg" alt="cit2" height="300px">
     <p>cena 20,99zł</p>
-    <input type="text" name="count2" id="count2" value="">
+    <input type="text" name="count2" id="count2" value="0">
     </div><div>
     <img src="fotki/manga3.jpg" alt="cit3" height="300px">
     <p>cena 20,99zł</p>
-    <input type="text" name="count3" id="count3" value="">
+    <input type="text" name="count3" id="count3" value="0">
     </div><div>
     <img src="fotki/manga4.jpg" alt="cit4" height="300px">
     <p>cena 20,99zł</p>
-    <input type="text" name="count4" id="count4" value="">
+    <input type="text" name="count4" id="count4" value="0">
     </div><div>
     <img src="fotki/manga5.jpg" alt="cit5" height="300px">
     <p>cena 20,99zł</p>
-    <input type="text" name="count5" id="count5" value="">
+    <input type="text" name="count5" id="count5" value="0">
     </div><div>
     <img src="fotki/manga6.jpg" alt="cit6" height="300px">
     <p>cena 20,99zł</p>
-    <input type="text" name="count6" id="count6" value="">
+    <input type="text" name="count6" id="count6" value="0">
     </div>
     <div class="a">
     <input type="submit" value="Submit"></div>
@@ -36,13 +36,21 @@
         session_start();
         $db = new mysqli("localhost", "root", "", "manga");
         //tutaj jakaś tabelka - poustawiać te county i tak dalej używając print_r
-        $cena1 = $db->query("SELECT cena1 FROM produkty");
-        $cena2 = $db->query("SELECT cena2 FROM produkty");
-        $cena3 = $db->query("SELECT cena3 FROM produkty");
-        $cena4 = $db->query("SELECT cena4 FROM produkty");
-        $cena5 = $db->query("SELECT cena5 FROM produkty");
-        $cena6 = $db->query("SELECT cena6 FROM produkty");
-        $total = $cena1 * $cena2 * $cena3 * $cena4 * $cena5 * $cena6;
+
+        if(isset($count)) {$_REQUEST['count'];}
+        if(isset($count2)) {$_REQUEST['count2'];}
+        if(isset($count3)) {$_REQUEST['count3'];}
+        if(isset($count4)) {$_REQUEST['count4'];}
+        if(isset($count5)) {$_REQUEST['count5'];}
+        if(isset($count6)) {$_REQUEST['count6'];}
+
+        $cena1 = 20;
+        $cena2 = 20;
+        $cena3 = 20;
+        $cena4 = 20;
+        $cena5 = 20;
+        $cena6 = 20;
+        $total = ($cena1 * $count) + ($cena2 * $count2) + ($cena3 * $count3)+ ($cena4 * $count4) + ($cena5 * $count5) + ($cena6 * $count6);
         $_SESSION['count'] = $_REQUEST['count'];
         $_SESSION['count2'] = $_REQUEST['count2'];
         $_SESSION['count3'] = $_REQUEST['count3'];
@@ -62,9 +70,8 @@
     <input type="text" name="adres" id="adres" value="adres">
     <input type="submit" value="Kup">
     <?php
-    $q = $db->prepare("INSERT INTO zamowienia VALUES(NULL,?,?,?,?,?,?,?,?");
-    $q->bind_param("iiiiiiis", $_SESSION['count'], $_SESSION['count2'], $_SESSION['count3'], $_SESSION['count4'], $_SESSION['count5'], $_SESSION['count6'], $_SESSION['total'], $_REQUEST['adres']);
+    $q = $db->prepare("INSERT INTO zamowienia VALUES(NULL,?,?,?,?,?,?,?,?)");
+    $q->bind_param("iiiiiiis", $count, $count2, $count3, $count4, $count5, $count6, $total, $_REQUEST['adres']);
     $q->execute();
-    var_dump($_SESSION['count'])
     ?>
 </form>
--- staffmode.php
+++ staffmode.php
@@ -1,7 +1,27 @@
 <link rel="stylesheet" href="style.css">
 <img class="baner" src="fotki/baner.png" alt="">
+
+<div class=wszystko>
 <?php //zaplecze staff 
      $db = new mysqli("localhost", "root", "", "manga");
-
+     $q = $db->prepare("SELECT * FROM zamowienia");
+     if($q->execute()) {
+          $result = $q->get_result();
+          while($row = $result->fetch_assoc()) {
+               $id = $row['id'];
+               $manga1 = $row['manga1'];
+               $manga2 = $row['manga2'];
+               $manga3 = $row['manga3'];
+               $manga4 = $row['manga4'];
+               $manga5 = $row['manga5'];
+               $manga6 = $row['manga6'];
+               $total = $row['total'];
+               $adres = $row['adres'];
+               echo "Zamówienia:<br>";
+               echo "id: $id, mangi: $manga1 $manga2 $manga3 $manga4 $manga5 $manga6, cena końcowa $total, adres: $adres<br>";
+          }
+     }
 
      ?>
+     </div>
+     
